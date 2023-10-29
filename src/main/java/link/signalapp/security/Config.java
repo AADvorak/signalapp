3740bf54026f78090930f0b25c72e60f7c542cfd
A. Dvorak <dvorak88@mail.ru>
2023-10-29 16:38:33 +0400

    fix: security config 401

--- src/main/java/link/signalapp/security/Config.java
+++ src/main/java/link/signalapp/security/Config.java
@@ -2,9 +2,11 @@ package link.signalapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -12,16 +14,17 @@ public class Config {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, TokenRequestFilter tokenRequestFilter) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/sessions").permitAll()
-                        .requestMatchers("/api/settings").permitAll()
-                        .requestMatchers("/api/modules").permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
-                        .anyRequest().authenticated());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/sessions").permitAll()
+                .requestMatchers("/api/settings").permitAll()
+                .requestMatchers("/api/modules").permitAll()
+                .requestMatchers("/api/users/**").permitAll()
+                .anyRequest().authenticated());
 
-        http
-                .addFilterBefore(tokenRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+
+        http.addFilterBefore(tokenRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.csrf(AbstractHttpConfigurer::disable);
 
