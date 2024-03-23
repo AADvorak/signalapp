0cdf2aa6ef2cdb24ff8a5b1a10b949fd43c8d376
A. Dvorak <dvorak88@mail.ru>
2024-03-23 13:34:04 +0400

    fix: FileManager remove unused methods

--- src/main/java/link/signalapp/file/FileManager.java
+++ src/main/java/link/signalapp/file/FileManager.java
@@ -1,17 +1,11 @@
 package link.signalapp.file;
 
 import link.signalapp.ApplicationProperties;
-import link.signalapp.SignalApplication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileSystemUtils;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,22 +14,9 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 public class FileManager {
 
-    private static final String MODULES_PATH = "modules/";
     private static final String SIGNALS_PATH = "signals/";
 
     private final ApplicationProperties applicationProperties;
-
-    public void writeModuleToFile(String module, String extension, String data) throws IOException {
-        String moduleLowerCase = module.toLowerCase();
-        String moduleDir = SignalApplication.FRONTEND_OLD_PATH + MODULES_PATH + moduleLowerCase;
-        createDirIfNotExists(moduleDir);
-        writeStringToFile(data, moduleDir + "/" + moduleLowerCase + "." + extension);
-    }
-
-    public String readModuleFromFile(String module, String extension) throws IOException {
-        return readStringFromFile(SignalApplication.FRONTEND_OLD_PATH + MODULES_PATH +
-                module + "/" + module + "." + extension);
-    }
 
     public byte[] readWavFromFile(int userId, int signalId) throws IOException {
         String wavFile = getWavDir(userId, signalId) + "/" + signalId + ".wav";
@@ -43,13 +24,6 @@ public class FileManager {
             return new byte[0];
         }
         return readBytesFromFile(wavFile);
-    }
-
-    public void writeBytesToWavFile(int userId, int signalId, AudioFormat format, byte[] bytes) throws IOException {
-        String wavDir = getWavDir(userId, signalId);
-        createDirIfNotExists(wavDir);
-        AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(bytes), format, bytes.length);
-        AudioSystem.write(ais, AudioFileFormat.Type.WAVE, new File(wavDir + "/" + signalId + ".wav"));
     }
 
     public void writeWavToFile(int userId, int signalId, byte[] bytes) throws IOException {
@@ -66,22 +40,12 @@ public class FileManager {
         deleteDirRecursively(new File(applicationProperties.getDataPath() + SIGNALS_PATH + userId));
     }
 
-    private void deleteDirRecursively(File dir) {
+    public void deleteDirRecursively(File dir) {
         FileSystemUtils.deleteRecursively(dir);
-    }
-
-    private void writeStringToFile(String string, String fileName) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write(string);
-        }
     }
 
     private void writeBytesToFile(byte[] bytes, String fileName) throws IOException {
         Files.write(Paths.get(fileName), bytes);
-    }
-
-    private String readStringFromFile(String fileName) throws IOException {
-        return new String(readBytesFromFile(fileName), StandardCharsets.UTF_8);
     }
 
     private byte[] readBytesFromFile(String fileName) throws IOException {
@@ -98,5 +62,4 @@ public class FileManager {
             Files.createDirectories(path);
         }
     }
-
 }
