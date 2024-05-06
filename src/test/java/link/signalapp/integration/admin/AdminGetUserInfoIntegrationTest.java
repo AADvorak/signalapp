983a6453f395a4ad12792d6c238879fdaf8f2b44
A. Dvorak <dvorak88@mail.ru>
2024-05-06 15:13:44 +0400

    feat: admin integration tests 2

---
+++ src/test/java/link/signalapp/integration/admin/AdminGetUserInfoIntegrationTest.java
@@ -0,0 +1,97 @@
+package link.signalapp.integration.admin;
+
+import link.signalapp.dto.response.UserDtoResponse;
+import link.signalapp.model.Signal;
+import link.signalapp.model.User;
+import link.signalapp.model.UserToken;
+import link.signalapp.repository.SignalRepository;
+import link.signalapp.repository.UserTokenRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AdminGetUserInfoIntegrationTest extends AdminIntegrationTestBase {
+
+    private static final int SECOND_USER_SIGNALS_NUMBER = 5;
+
+    @Autowired
+    private SignalRepository signalRepository;
+
+    @Autowired
+    private UserTokenRepository userTokenRepository;
+
+    @BeforeAll
+    public void beforeAll() {
+        userRepository.deleteAll();
+        registerUsers();
+        giveAdminRoleToUser(email1);
+        signalRepository.deleteAll();
+        createSignalsInDB();
+    }
+
+    @Test
+    public void getFirstUserInfo() {
+        ResponseEntity<UsersPage> response = template.exchange(fullUrl(FILTER_USERS_URL), HttpMethod.POST,
+                new HttpEntity<>(createUserFilterDto().setSearch(email1), login(email1)), UsersPage.class);
+        UserDtoResponse userDtoResponse = Objects.requireNonNull(response.getBody()).getData().get(0);
+        User user = userRepository.findByEmail(email1);
+        UserToken userToken = userTokenRepository.findLastByUserId(user.getId());
+        assertNotNull(user.getRole());
+        assertAll(
+                () -> assertEquals(user.getId(), userDtoResponse.getId()),
+                () -> assertEquals(user.getEmail(), userDtoResponse.getEmail()),
+                () -> assertEquals(user.getFirstName(), userDtoResponse.getFirstName()),
+                () -> assertEquals(user.getLastName(), userDtoResponse.getLastName()),
+                () -> assertEquals(user.getPatronymic(), userDtoResponse.getPatronymic()),
+                () -> assertEquals(user.getPatronymic(), userDtoResponse.getPatronymic()),
+                () -> assertEquals(0, userDtoResponse.getStoredSignalsNumber()),
+                () -> assertEquals(user.getCreateTime(), userDtoResponse.getCreateTime()),
+                () -> assertEquals(userToken.getLastActionTime(), userDtoResponse.getLastActionTime()),
+                () -> assertEquals(getAdminRole().getId(), userDtoResponse.getRole().getId())
+        );
+    }
+
+    @Test
+    public void getSecondUserInfo() {
+        ResponseEntity<UsersPage> response = template.exchange(fullUrl(FILTER_USERS_URL), HttpMethod.POST,
+                new HttpEntity<>(createUserFilterDto().setSearch(email2), login(email1)), UsersPage.class);
+        UserDtoResponse userDtoResponse = Objects.requireNonNull(response.getBody()).getData().get(0);
+        User user = userRepository.findByEmail(email2);
+        UserToken userToken = userTokenRepository.findLastByUserId(user.getId());
+        assertAll(
+                () -> assertEquals(user.getId(), userDtoResponse.getId()),
+                () -> assertEquals(user.getEmail(), userDtoResponse.getEmail()),
+                () -> assertEquals(user.getFirstName(), userDtoResponse.getFirstName()),
+                () -> assertEquals(user.getLastName(), userDtoResponse.getLastName()),
+                () -> assertEquals(user.getPatronymic(), userDtoResponse.getPatronymic()),
+                () -> assertEquals(user.getPatronymic(), userDtoResponse.getPatronymic()),
+                () -> assertEquals(SECOND_USER_SIGNALS_NUMBER, userDtoResponse.getStoredSignalsNumber()),
+                () -> assertEquals(user.getCreateTime(), userDtoResponse.getCreateTime()),
+                () -> assertEquals(userToken.getLastActionTime(), userDtoResponse.getLastActionTime()),
+                () -> assertNull(user.getRole())
+        );
+    }
+
+    private void createSignalsInDB() {
+        int userId = userRepository.findByEmail(email2).getId();
+        for (int i = 1; i <= SECOND_USER_SIGNALS_NUMBER; i++) {
+            signalRepository.save(new Signal()
+                    .setName("name" + i)
+                    .setDescription("description" + i)
+                    .setSampleRate(BigDecimal.valueOf(8000))
+                    .setXMin(BigDecimal.ZERO)
+                    .setMaxAbsY(BigDecimal.ONE)
+                    .setUserId(userId));
+        }
+    }
+}
