49d6b6f2dd826e05cd22d17baffac7b06e5aea4f
A. Dvorak <dvorak88@mail.ru>
2024-05-07 14:56:50 +0400

    fix: SignalService unused imports

--- src/main/java/link/signalapp/service/SignalService.java
+++ src/main/java/link/signalapp/service/SignalService.java
@@ -20,9 +20,7 @@ import link.signalapp.repository.SignalRepository;
 import link.signalapp.service.utils.FilterUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
