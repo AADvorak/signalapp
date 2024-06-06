49278457a4a3aa90827dd6437052c89f4b325314
A. Dvorak <dvorak88@mail.ru>
2024-06-06 13:53:13 +0400

    fix: equals in composite id classes, spring.jpa.open-in-view=false

--- src/main/java/link/signalapp/model/SignalInFolderId.java
+++ src/main/java/link/signalapp/model/SignalInFolderId.java
@@ -2,6 +2,7 @@ package link.signalapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 @Embeddable
 public class SignalInFolderId implements Serializable {
 
